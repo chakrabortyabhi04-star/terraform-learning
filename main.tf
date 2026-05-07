3ce2ae2830b5b2e6905b73fceaa97d04a4bc4f55
Abhishek Chakraborty <chakraborty.abhi04@gmail.com>
2026-05-07 12:24:20 +0530

    Day 3 - Virtual Network resource created

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "terraformlearning" {
   name     = "rg-terraform-learning"
   location = "West Europe"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet_learning"
+  location            = azurerm_resource_group.terraformlearning.location
+  resource_group_name = azurerm_resource_group.terraformlearning.name
+  address_space       = ["10.0.0.0/16"]
+}
