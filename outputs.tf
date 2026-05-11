cbafc870928c232363575b3ca99848acae6de1ac
Abhishek Chakraborty <chakraborty.abhi04@gmail.com>
2026-05-11 21:09:47 +0530

    Day 9: Add output values for vnet_id and rg_id

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "vnet_id" {
+  value       = azurerm_virtual_network.vnet.id
+  description = "Azure Virtual network address"
+}
+
+output "rg.id" {
+  value       = azurerm_resource_group.terraformlearning.id
+  description = "Azure resource group name"
+}
