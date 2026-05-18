539cb726d87c50208cf20bf5b6ab999cfa47e068
Abhishek Chakraborty <chakraborty.abhi04@gmail.com>
2026-05-18 23:26:39 +0530

    Day 15: Production naming convention using common_prefix local

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "terraformlearning" {
-  name     = var.resource_group_name
+  name     = "rg-${local.common_prefix}"
   location = var.location
   tags = local.common_tags
   
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "terraformlearning" {
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.terraformlearning.name
   address_space       = var.vnet_address_space
   tags = local.common_tags
   
