72ca11b391be6628ebf983bd048a28ca845371f0
Abhishek Chakraborty <chakraborty.abhi04@gmail.com>
2026-05-19 18:49:52 +0530

    Day 16: Production VNet naming using common_prefix

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "terraformlearning" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
+  name                = "vnet-${local.common_prefix}"
   location            = var.location
   resource_group_name = azurerm_resource_group.terraformlearning.name
   address_space       = var.vnet_address_space
