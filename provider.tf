d80b9f407f86af78f69bc4bb64f0f08dba57cdeb
Abhishek Chakraborty <chakraborty.abhi04@gmail.com>
2026-06-01 11:18:29 +0530

    Day 32 — Remote Backend: Azure Storage (Hands-On Verification)

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.69.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "tfstateabhishek2024"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
@@ -12,3 +19,5 @@ provider "azurerm" {
 
  
 }
+
+
